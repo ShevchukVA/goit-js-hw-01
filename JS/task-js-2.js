8a6f6a27d2798bc312c0a7058c6bba33843dfb36
ShevchukVA <60571733+ShevchukVA@users.noreply.github.com>
2020-05-20 11:57:57 +0300

    Cansel

--- JS/task-js-2.js
+++ JS/task-js-2.js
@@ -2,8 +2,14 @@ const total = 100;
 
 const ordered = prompt('Введите количество товара');
 
-console.log('Ваш заказ:', ordered);
+if (ordered === null) {
+  console.log('Отмена заказа товара');
+} else {
+  console.log('Ваш заказ:', ordered);
 
-ordered > total
-  ? console.log('На складе недостаточно товаров!')
-  : console.log('Заказ оформлен, с вами свяжется менеджер.');
+  if (ordered > total) {
+    console.log('На складе недостаточно товаров!');
+  } else {
+    console.log('Заказ оформлен, с вами свяжется менеджер');
+  }
+}
--- JS/task-js-5.js
+++ JS/task-js-5.js
@@ -1,31 +1,35 @@
 let price;
 let country = prompt('Введите вашу страну:');
 
-country = country.toLowerCase();
+if (country === null) {
+  console.log('Отменено пользователем');
+  country = ' ';
+  price = ' ';
+} else {
+  country = country.toLowerCase();
+  switch (country) {
+    case 'китай':
+      price = 100;
+      break;
 
-switch (country) {
-  case 'китай':
-    price = 100;
-    break;
+    case 'чили':
+      price = 250;
+      break;
 
-  case 'чили':
-    price = 250;
-    break;
+    case 'австралия':
+      price = 170;
+      break;
 
-  case 'австралия':
-    price = 170;
-    break;
+    case 'индия':
+      price = 80;
+      break;
 
-  case 'индия':
-    price = 80;
-    break;
+    case 'ямайка':
+      price = 120;
+      break;
 
-  case 'ямайка':
-    price = 120;
-    break;
-
-  default:
-    alert('В вашей стране доставка не доступна');
+    default:
+      alert('В вашей стране доставка не доступна');
+  }
 }
-
 console.log(`Доставка в '${country}' будет стоить '${price}' кредитов.`);
